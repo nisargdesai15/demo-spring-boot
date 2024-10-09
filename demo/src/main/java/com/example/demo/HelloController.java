311c8e7fc041a96ad29392bd289eeb8cb0a31052
Nisarg Desai <nisargdesai15@users.noreply.github.com>
2024-10-09 10:30:29 -0500

    Update HelloController.java

--- demo/src/main/java/com/example/demo/HelloController.java
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -3,8 +3,12 @@ package com.example.demo;
 
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 public class HelloController {
@@ -16,6 +20,10 @@ public class HelloController {
 
     private final MovieService movieService;
 
+    @Autowired
+    private AddressVerificationService addressVerificationService;
+
+
     public HelloController(MovieService movieService) {
         this.movieService = movieService;
     }
@@ -24,4 +32,10 @@ public class HelloController {
     public String getMovies() {
         return movieService.getMovies();
     }
+
+    @GetMapping("/verify-address")
+    public String verifyAddress(@RequestParam String address) throws IOException {
+        return addressVerificationService.verifyAddress(address);
+    }
+
 }
